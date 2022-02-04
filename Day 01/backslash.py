4cd63ea8ada8646a9ffa0e0e0aa63a1ecbc8b65c
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-04 15:05:30 +0530

    added backslash notes

---
+++ Day 01/backslash.py
@@ -0,0 +1,37 @@
+print(" backslash n is used to have a line break and shift the afterward content to next line in the compiler !!!")
+
+#showing the use cases of the \n 
+print('''
+
+showing the use cases of the backslash
+
+like if i want to print "Hello World !" 4 times 
+
+1st Approach is to do this:
+
+print("Hello World !")
+print("Hello World !")
+print("Hello World !")
+print("Hello World !")
+
+2nd Approach is to  do use backslash:
+print("Hello World !{backslash}Hello World !{backslash}Hello World !{backslash}Hello World !")
+
+Lets see the Outputs for the following :
+
+
+
+''')
+print("1st Approach")
+
+print("Hello World !")
+print("Hello World !")
+print("Hello World !")
+print("Hello World !")
+
+print("2nd  Approach")
+print("Hello World !\nHello World !\nHello World !\nHello World !")
+
+# no difference in the output
+
+print(" You can see there is no Difference in the output but we have difference in the code")
