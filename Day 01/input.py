5bce7a3dc0ad84e742b91d8d4444eb8e0f50516f
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-04 16:06:24 +0530

    added input functions concepts

---
+++ Day 01/input.py
@@ -0,0 +1,50 @@
+#                                              Input Functions Overview
+
+print("                                             Input Functions Overview")
+print("""
+There is most of time that you want to take a input from the user and then do something with that and give the output !
+
+so How do we take Inputs from the user in python ?  
+
+                            -> In python we do that by calling Input function
+---------------------------------------------------------------------------------------------------------------------------------------
+- Sytntax Goes like this :
+
+
+                    input("The message on the behalf of which you want the Input from The user !")
+
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+
+                                                   >PRINT VS INPUT<
+
+    When we call the print function the job is to print the stuff and move to next line of code whereas , in case of Input 
+    function the the stuff is simply printed like print function but in addtion to that it want a input from the user and 
+    return that in the code and then move to next line of code .
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+
+                                                      Input () Function
+
+                                                 input("A promot for the user") 
+
+    Also , we can store the input from the user into some thing called variables(refer to variables.py)
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+
+                                               Stored_input=input("HEY READER!")
+
+    The code above actually take the input from the user on the behalf of the msg {HEY READER!} and send it to code and python store 
+    that in the Variable called Stored_input
+
+---------------------------------------------------------------------------------------------------------------------------------------
+                                        print ("Hello " + input ("What is your name?"))
+
+    The code above simply  print the String Hello  and then it moves to second part for sting Concatenation  , then it promot for the input from the user
+    giving the meassge   -> What is your name? <-  the input from the user is replaced into  -> input ("What is your name?") <-  and then 
+    sting Concatenation is done and finally the output is given by the print function
+
+    >>>What is your name?Abhishek #Here Abhishek is the input from the user
+    >>>Hello Abhishek ! #The Output we get from the coded code
+
+""")
