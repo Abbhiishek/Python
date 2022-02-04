35de23eb296a4cebcc8fd65e4c005f6d37988a5e
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-04 15:15:48 +0530

    Excercise 01 submitted

---
+++ Day 01/Excercise/Exercise 1 - Printing.py
@@ -0,0 +1,47 @@
+
+#                                 codingrooms.com assignments EXcercise 01
+
+
+print('''
+codingrooms.com assignments EXcercise 01
+
+
+Example Output
+After you have written your code, you should run your program and it should print the following:
+
+Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')
+
+''')
+
+#                                    Write your code below this line 👇
+
+
+
+print(" \n Excercise 01 from CodingRooms 👇 \n ") #\n is blackslash and it is used to have a line break and shift the afterward content to next line in the compiler !!!  
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+
+#                                   What i have learn't from this
+
+
+
+
+print(
+'''
+What i have learn't from this :
+
+Its appers that the whole 3 lines are in single block but they aree in different as they load with some time 
+
+in line { print("print('what to print')") } we have used '' instead of "" as we can use both but we have to make sure that the opening and closing  quotation should be same , as in this line we want to print a ' ' so we used " " as opening and closing quotes  
+
+
+alternatively we can use vice versa .......
+
+
+Also , blackslash n is used to have a line break and shift the afterward content to next line in the compiler !!!  
+
+''')
