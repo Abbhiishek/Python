66862a56a288c1c932e4c74ed3cb8c3bc24f60ad
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-04 17:19:24 +0530

    changed the notes of print function

--- Day 01/printing.py
+++ Day 01/printing.py
@@ -17,42 +17,14 @@ THE "" SHOULD HE OPENED AND CLOSED
 
 LIKE : print("this will not run and give error as we didn't close the value we want to pass )
 
-''')
 
 
-
-# String Manipulation and Code Intelligence
-print("String Manipulation")
-
-print('''
-
-------------------------------------------------------------------------------------------
-
-We can combine two string in print statement using '+' e.g. print("Hello"+"World")
-
-Doing this the word Hello World is printed as HelloWorld no spaces are there in between but we can make it happer 
+SomeThing to Ponder:
+   - Print statement not only print strings it can print any value stored in the variable for that :
+      we just have to place that variable name in the brackets without quotation!
 
 
-1st approach :
-
-------- >>> print("Hello "+"World")
-
-- The first approach is to add a space after Hello within the quotation to make it in considerations
-
-2nd approach :
-
-------- >>> print("Hello"+"World ")
-
-- The second approach is to add a space after World within the quotation to make it in considerations
-
-3rd  approach :
-
-------- >>> print("Hello" + " " + "World ")
-
-- The third approach is to add a blank space within quotes around with + to combine three different Strings in one place to make it in considerations.
-- In this case the whole thing is focused in the addon as the separter in nothing but a blank space added within a quotes.
-
-
+      cooool man !
 
 ''')
 
