23e5e221957fa75f4215953b7e4018ade22f8e4d
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-04 15:04:43 +0530

    Print function

---
+++ Day 01/printing.py
@@ -0,0 +1,58 @@
+print("This is a Print Statment.")
+
+# This is a print functions (predefined functions! )
+print('''
+
+This is a print functions (predefined functions! )
+
+
+print is the function name and inside the paranstes we pass the thing we want to print.
+
+
+we have used " " to tell the system that its just a text that we have to print it's not hard coded code 
+
+its have to be {STRINGS}
+
+THE "" SHOULD HE OPENED AND CLOSED 
+
+LIKE : print("this will not run and give error as we didn't close the value we want to pass )
+
+''')
+
+
+
+# String Manipulation and Code Intelligence
+print("String Manipulation")
+
+print('''
+
+------------------------------------------------------------------------------------------
+
+We can combine two string in print statement using '+' e.g. print("Hello"+"World")
+
+Doing this the word Hello World is printed as HelloWorld no spaces are there in between but we can make it happer 
+
+
+1st approach :
+
+------- >>> print("Hello "+"World")
+
+- The first approach is to add a space after Hello within the quotation to make it in considerations
+
+2nd approach :
+
+------- >>> print("Hello"+"World ")
+
+- The second approach is to add a space after World within the quotation to make it in considerations
+
+3rd  approach :
+
+------- >>> print("Hello" + " " + "World ")
+
+- The third approach is to add a blank space within quotes around with + to combine three different Strings in one place to make it in considerations.
+- In this case the whole thing is focused in the addon as the separter in nothing but a blank space added within a quotes.
+
+
+
+''')
+
