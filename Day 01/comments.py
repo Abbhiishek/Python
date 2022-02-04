a7b8abbf0d2dd0987efcc5da0c8dfca224719073
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-04 16:05:58 +0530

    added comments concept

---
+++ Day 01/comments.py
@@ -0,0 +1,36 @@
+#                                    Commenting
+
+print("                                    Commenting")
+
+print('''
+=========================================================================================================================
+
+
+- if we want a line in the code that to be avoided by the interpreter and will not be considered while running the program
+  the Commenting Concept comes in ....
+
+- We just have to add a # in front of line to make that line comment
+
+- If we use # in any line where some code is written then after the # all the things will be not considered 
+  like : 
+     print("hello" + #this is a comment)
+
+    In this code after the # all the things will be considered as the comments and not to be considered while running
+    as in the code the closing bracket is also after the # the code give the Error : SyntaxError
+
+    - So its advised to use comments in new separate line or after the code is finished for that particular line .
+
+- We have also a option of multi line commenting which comes handy when we have to comment a large content 
+ 
+-  we can use """ {the content to 
+          comments, supports multiline } """  , you can use single quote thrice also as now i am using that for 
+          print statement (refer to printing.py), will give errors
+
+=========================================================================================================================
+                             So what's the practical use of the commenting ?
+
+        1. actually comments are used to refer to the task that the code do 
+        2. You can use that for you well wish.
+        3. It can be used to Markdown something in the code , to make some notes , marking some code changes and so on ...
+        
+''')
