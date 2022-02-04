37e912a35fc4b0b0e4ec8b225b209a4a3e38b245
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-04 15:03:09 +0530

    Indentations

---
+++ Indentations.py
@@ -0,0 +1,17 @@
+#Indentations IN PYTHON 
+
+print("Indentations IN PYTHON  👇 ")
+
+print('''
+
+In python Programming language we have spaces and that's matter the most (also known as Indentations)
+
+like if we start our print statement not from the start of a line  , rather after some spaces 
+
+like 
+   print("Hello World !")
+- This is going to give us a error called : IndentationError!
+- Indentation is one of the success key to have python !
+
+
+''')
