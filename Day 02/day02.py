40d15fed6c98aa7da53f6a7fb52979deff209008
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-12 23:44:13 +0530

    Added the goal for day 02 🎊

--- Day 02/day02.py
+++ Day 02/day02.py
@@ -1,1 +1,23 @@
-#                                                                 day 02 starts 
+#                                day 02 starts 
+
+print(
+    """                                                    😀😀 Welcome To Day 02 Of Leraning Python 😀😀
+
+                                                                       Topics Covered : 😎
+                                                                       1.Len Function
+                                                                       2.Datatype - Strings , Number , Boolean
+                                                                       3.Type conversion and typecasting
+                                                                       4.str() , int() , float()
+                                                                       5.EXcercise 01
+                                                                       6. Mathematical Operators
+                                                                       7.Concept of PEMDAS.LR
+                                                                       8.EXcercise 02 - BMI
+                                                                       9.round() function 
+                                                                       10.F String
+                                                                       11.Exercise 03
+                                                                       12.TIP CALCULATOR - Project I
+                                                    
+
+
+    """
+)
