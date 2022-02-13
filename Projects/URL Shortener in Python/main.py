be54dd62065fa5ec2240d55f7c9aa7f835e140e6
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-14 00:46:28 +0530

    added the url shortner

--- Projects/URL Shortener in Python/main.py
+++ Projects/URL Shortener in Python/main.py
@@ -1,1 +1,17 @@
 import pyshorteners
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+
+
+long_url = input("Enter the URL to shorten: ")
+ 
+#Bitly shortener service
+type_bitly = pyshorteners.Shortener(api_key=os.getenv('url_shortner'))
+short_url = type_bitly.bitly.short(long_url)
+count = type_bitly.bitly.total_clicks(short_url) #gives total no. of clicks.
+
+print("The Shortened URL is: " + short_url)
+print("The Total Number of Clicks is: " +str(count))
