90e3b43747b9177d5e21162b73561059e8983e07
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-13 16:50:13 +0530

    added the excerice 03 problem

---
+++ Day 02/Excercise/2.3.py
@@ -0,0 +1,26 @@
+#                                 codingrooms.com assignments EXcercise 03
+
+
+print('''
+codingrooms.com assignments EXcercise 03
+
+
+Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
+
+It will take your current age as the input and output a message with our time left in this format:
+
+You have x days, y weeks, and z months left.
+
+Where x, y and z are replaced with the actual calculated numbers.
+
+Warning your output should match the Example Output format exactly, even the positions of the commas and full stops.
+
+Example Input
+56
+Example Output
+You have 12410 days, 1768 weeks, and 408 months left.
+
+''')
+
+#                                    Write your code below this line 👇
+
