58d26085d580895b07be43c6e441d3c162850550
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-02-16 12:39:39 +0530

    rounded

--- Day 03/Exercise/3.2.py
+++ Day 03/Exercise/3.2.py
@@ -46,11 +46,11 @@ weight = float(input("enter your weight in kg: "))
 
 #Write your code below this line 👇
 
-bmi=round(weight/(height**2),1)
+bmi=round(weight/(height**2))
 if bmi<18.5:
     print(f"Your BMI is {bmi}, you are underweight.")
 elif bmi<25:
-    print(f"Your BMI is {bmi}, you are underweight.")
+    print(f"Your BMI is {bmi}, you have a normal weight.")
 elif bmi<30:
     print(f"Your BMI is {bmi}, you are slightly overweight.")
 elif bmi<35:
@@ -70,11 +70,11 @@ weight = float(input("enter your weight in kg: "))
 
 #Write your code below this line 👇
 
-bmi=round(weight/(height**2),1)
+bmi=round(weight/(height**2))
 if bmi<18.5:
     print(f"Your BMI is {bmi}, you are underweight.")
 elif bmi<25:
-    print(f"Your BMI is {bmi}, you are underweight.")
+    print(f"Your BMI is {bmi}, you have a normal weight.")
 elif bmi<30:
     print(f"Your BMI is {bmi}, you are slightly overweight.")
 elif bmi<35:
