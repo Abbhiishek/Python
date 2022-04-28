957f6b55a5a7fc394bfa0477a6fe86e1e77ebf9d
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2022-04-28 12:16:15 +0530

    🙄

--- Day Pogress - 18~100/Day 13/main.py
+++ Day Pogress - 18~100/Day 13/main.py
@@ -12,8 +12,6 @@ def my_function():
             print("You got it")
 my_function()
 
-
-
 # Reproduce the Bug
 from random import randint
 dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
@@ -35,8 +33,8 @@ print(dice_imgs[dice_num])
 
 # Play Computer
 year = int(input("What's your year of birth?"))
-if year > 1980 and year < 1994:
-  print("You are a millenial.")
+if 1980 < year < 1994:
+    print("You are a millenial.")
 elif year > 1994:
     """
     if year > 1994:
@@ -59,13 +57,13 @@ age = input("How old are you?")
 the default value of age is in string so we have to convert ingto inrt
 .
 """
-#solution
+# solution
 age = int(age)
 if age > 18:
     # print("You can drive at age {age}.")
     print(f"You can drive at age {age}.")
 
-#Print is Your Friend
+# Print is Your Friend
 pages = 0
 word_per_page = 0
 pages = int(input("Number of pages: "))
@@ -74,12 +72,14 @@ word_per_page = int(input("Number of words per page: "))
 total_words = pages * word_per_page
 print(total_words)
 
-#Use a Debugger
-def mutate(a_list):
-  b_list = []
-  for item in a_list:
-    new_item = item * 2
-    b_list.append(new_item)
-  print(b_list)
 
-mutate([1,2,3,5,8,13])
+# Use a Debugger
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item)
+    print(b_list)
+
+
+mutate([1, 2, 3, 5, 8, 13])
